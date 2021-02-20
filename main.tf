bb913849d720473f49d4a489381a8ddabfb104f7
crycrime <crycrime@gmail.com>
2021-02-20 23:42:16 +0200

    add key

--- main.tf
+++ main.tf
@@ -63,6 +63,7 @@ resource "aws_instance" "ec2_ansible" {
   ami                    = "ami-0e80a462ede03e653" #Amazon Linux 2 AMI (HVM)
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ansible_sg.id]
+  key_name               = "terraform_for"
 
   tags = {
     Name = "Instance with wordpress"
